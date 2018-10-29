0b58a00ca02c0f70f4cc9eeb33af5dca5973643f
WangYihang <wangyihanger@gmail.com>
2018-10-29 15:38:19 +0000

    [+] Use git submodules

--- challenge/do-you-know-phar/source/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-if(isset($_GET['page'])){
-	$file = $_GET['page'].'.php';
-	include($file);
-}else{
-	header("Location: /?page=login");
-	die();
-}
-?>
--- writeup/do-you-know-phar/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-$phar = new Phar('c.phar', 0, 'c.phar');
-$phar->buildFromDirectory('./c');
-$phar->setStub($phar->createDefaultStub('e.php', ' => .php'));
-$phar->compressFiles(Phar::GZ);
-?>
